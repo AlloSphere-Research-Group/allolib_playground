8e31b104191c84e1dd76f5dbcbdc0143615d85df
Andres Cabrera <andres@office>
2019-10-07 10:01:48 -0700

    Initial version of the playground

---
+++ examples/av/audioToGraphics.cpp
@@ -0,0 +1,94 @@
+/*
+Allocore Example: Audio To Graphics
+
+Description:
+This example demonstrates how to visualize real-time audio. It uses a single
+ring buffer to buffer audio samples between the audio and graphics threads. Two
+sine waves are generated in the audio thread and drawn as a Lissajous curve in
+the graphics thread.
+
+Author:
+Lance Putnam, 10/2012, putnam.lance@gmail.com
+*/
+
+#include "al/app/al_App.hpp"
+#include "al/types/al_SingleRWRingBuffer.hpp"
+
+using namespace al;
+
+// This example shows how to use a Buffer class to pass data from the
+// audio context to the graphics context.
+
+class MyApp : public App{
+public:
+
+  const size_t bufferSize = 8192;
+
+  float bufferData[8192];
+  double phase = 0;
+  // Create ring buffer with size 2048, we will use this buffer to inter
+  // leave the stereo samples from the audio callback
+  SingleRWRingBuffer ringBuffer{bufferSize * sizeof (float)};
+  Mesh curve;
+
+  void onCreate()
+  {
+    nav().pos(0,0,4);
+  }
+
+  // Audio callback
+  void onSound(AudioIOData& io){
+
+    // Set the base frequency to 55 Hz
+    double freq = 55/io.framesPerSecond();
+    float out[2];
+
+    while(io()){
+
+      // Update the oscillators' phase
+      phase += freq;
+      if(phase > 1) phase -= 1;
+
+      // Generate two sine waves at the 5th and 4th harmonics
+      out[0] = cos(5*phase * 2*M_PI);
+      out[1] = sin(4*phase * 2*M_PI);
+
+      // Write the waveforms to the ring buffer.
+      ringBuffer.write((const char *) out, 2 * sizeof (float));
+
+      // Send scaled waveforms to output...
+      io.out(0) = out[0]*0.2f;
+      io.out(1) = out[1]*0.2f;
+    }
+  }
+
+
+  void onAnimate(double dt){
+
+    curve.primitive(Mesh::LINE_STRIP);
+    curve.reset();
+
+    size_t samplesRead = ringBuffer.read((char *) bufferData, bufferSize * sizeof (float));
+
+    // Now we read samples from the buffer into the meash to be displayed
+    for(size_t i=0; i < samplesRead/sizeof (float); i = i+2){
+      curve.vertex(bufferData[i], bufferData[i+1]);
+      // The redder the lines, the closer we are to a full ring buffer
+      curve.color(HSV(0.5 *float(bufferSize)/(bufferSize - i)));
+    }
+  }
+
+  void onDraw(Graphics& g){
+    g.clear(0);
+    g.meshColor();
+    g.draw(curve);
+  }
+};
+
+
+int main(){
+  MyApp app;
+  app.configureAudio(); // init with out only
+  app.start();
+  return 0;
+}
---
+++ examples/av/helloVirtualWorld.cpp
@@ -0,0 +1,154 @@
+/*
+What is our "Hello world!" app?
+
+An agent orbits around the origin emitting the audio line input. The camera
+view can be switched between a freely navigable keyboard/mouse controlled mode
+and a sphere follow mode.
+
+Requirements:
+2 channels of spatial sound
+2 windows, one front view, one back view
+stereographic rendering
+*/
+
+#include "al/app/al_App.hpp"
+#include "al/graphics/al_Shapes.hpp"
+#include "al/math/al_Random.hpp"
+//#include "al/sound/al_Ambisonics.hpp"
+#include "al/scene/al_DynamicScene.hpp"
+
+#include <cmath>
+#include <iostream>
+
+using namespace al;
+
+#define AUDIO_BLOCK_SIZE 256
+
+// Create an agent,
+struct Agent : PositionedVoice, Nav {
+
+  float oscPhase {0}, oscFreq {220.0}, speed;
+
+  void onProcess(AudioIOData& io) override {
+    // Play a sine tone
+    while (io()) {
+      float s = std::sin(oscPhase * M_2PI);
+      oscPhase += oscFreq / io.framesPerSecond();
+      if (oscPhase >= 1) oscPhase -= 1;
+      io.out(0) = s * 0.1f;
+    }
+  }
+
+  void update(double dt) override
+  {
+    // Update internal smoothed nav
+    smooth(0.9);
+    spin(M_2PI / 360, M_2PI / 397, 0);
+    moveF(speed);
+    step();
+    // Now update the current agent's position from the smoothed nav
+    setPose(*this);
+  }
+
+  void onProcess(Graphics& g) override {
+
+    // draw
+    g.pushMatrix(/*Mat4f{Pose::matrix()}*/);
+
+    Mesh m{Mesh::TRIANGLES};
+    float ds = -0.5;
+
+    m.color(1, 1, 1);
+    m.vertex(0, 0, ds * 2);
+    m.color(1, 1, 0);
+    m.vertex(ds / 2, 0, -ds);
+    m.color(1, 0, 1);
+    m.vertex(-ds / 2, 0, -ds);
+
+    m.color(1, 1, 1);
+    m.vertex(0, 0, ds * 2);
+    m.color(0, 1, 1);
+    m.vertex(0, ds / 2, -ds);
+    m.color(1, 0, 1);
+    m.vertex(-ds / 2, 0, -ds);
+
+    m.color(1, 1, 1);
+    m.vertex(0, 0, ds * 2);
+    m.color(1, 1, 0);
+    m.vertex(ds / 2, 0, -ds);
+    m.color(0, 1, 1);
+    m.vertex(0, ds / 2, -ds);
+
+    g.meshColor();
+    g.draw(m);
+    g.popMatrix();
+  }
+
+};
+
+
+struct MyApp : App
+{
+
+  DynamicScene scene;
+  void onCreate() override {
+    // Set initial pose
+    nav() = {Vec3d(0,0,50), 0.95};
+
+    auto us10 = [] { return 10.0 * rnd::uniformS(); };
+
+    for (unsigned i = 0; i < 4; ++i) {
+      auto* ai = scene.getVoice<Agent>();
+      ai->oscFreq = 220.0f + (us10() * 220.0f);
+      ai->speed = 0.1f + 0.1f * rnd::uniform();
+      // randomize position and orientation
+      // We can do it this way because the agent inherits from Nav
+      ai->Pose::pos(us10(), us10(), us10());
+      ai->faceToward({us10(), us10(), us10()});
+      // Now insert into the scene
+      scene.triggerOn(ai);
+    }
+
+    // Make distance changes more noticeable
+    scene.distanceAttenuation().law(AttenuationLaw::ATTEN_INVERSE_SQUARE);
+  }
+
+  void onAnimate(double dt) override {
+    // Uncomment this line to make listener pose be the viewing pose;
+//    scene.listenerPose(pose());
+    scene.update(dt);
+  }
+
+  void onDraw(Graphics& g) override {
+    g.clear(0);
+    g.pushMatrix();
+    g.scale(10);
+    Mesh wb;
+    addWireBox(wb);
+    g.color(1);
+    g.draw(wb);
+    g.popMatrix();
+
+    scene.render(g);
+  }
+
+//  void onKeyDown(const Keyboard& k) override {
+//    // if (k.key() == Keyboard::TAB) {
+//    //  stereo.stereo(!stereo.stereo());
+//    // }
+//  }
+
+  void onSound(AudioIOData& io) override {
+    scene.render(io);
+  }
+
+};
+
+int main() {
+
+  MyApp win;
+  win.configureAudio(44100, AUDIO_BLOCK_SIZE, 2, 0);
+  win.start();
+
+  return  0;
+}
