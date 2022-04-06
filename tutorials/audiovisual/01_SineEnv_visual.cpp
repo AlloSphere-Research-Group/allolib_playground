3c4815b486ce9dfd657df0b25cd56dd4a0d28273
ben <benedic.lee@gmail.com>
2022-04-06 13:38:43 -0700

    Audiovisual instruments examples

---
+++ tutorials/audiovisual/01_SineEnv_visual.cpp
@@ -0,0 +1,305 @@
+﻿// MUS109IA & MAT276IA.
+// Spring 2022
+// Course Instrument 01. Sine Envelope
+// Myungin Lee
+
+#include <cstdio> // for printing to stdout
+#include <stdio.h>
+
+#include "Gamma/Analysis.h"
+#include "Gamma/Effects.h"
+#include "Gamma/Envelope.h"
+#include "Gamma/Oscillator.h"
+
+#include "al/app/al_App.hpp"
+#include "al/graphics/al_Shapes.hpp"
+#include "al/scene/al_PolySynth.hpp"
+#include "al/scene/al_SynthSequencer.hpp"
+#include "al/ui/al_ControlGUI.hpp"
+#include "al/ui/al_Parameter.hpp"
+#include "al/io/al_MIDI.hpp"
+#include "al/math/al_Random.hpp"
+
+// using namespace gam;
+using namespace al;
+using namespace std;
+
+// This example shows how to use SynthVoice and SynthManagerto create an audio
+// visual synthesizer. In a class that inherits from SynthVoice you will
+// define the synth's voice parameters and the sound and graphic generation
+// processes in the onProcess() functions.
+
+Vec3f randomVec3f(float scale)
+{
+  return Vec3f(al::rnd::uniformS(), al::rnd::uniformS(), al::rnd::uniformS()) * scale;
+}
+
+
+class SineEnv : public SynthVoice
+{
+public:
+  // Unit generators
+  gam::Pan<> mPan;
+  gam::Sine<> mOsc;
+  gam::Env<3> mAmpEnv;
+  // envelope follower to connect audio output to graphics
+  gam::EnvFollow<> mEnvFollow;
+  double a = 0;
+  double b = 0;
+  double timepose = 0;
+  Vec3f note_position;
+  Vec3f note_direction;
+
+  // Additional members
+  Mesh mMesh;
+  // Initialize voice. This function will only be called once per voice when
+  // it is created. Voices will be reused if they are idle.
+  void init() override
+  {
+    // Intialize envelope
+    mAmpEnv.curve(0); // make segments lines
+    mAmpEnv.levels(0, 1, 1, 0);
+    mAmpEnv.sustainPoint(2); // Make point 2 sustain until a release is issued
+
+    // We have the mesh be a sphere
+    addSphere(mMesh, 0.3, 50, 50);
+    mMesh.decompress();
+    mMesh.generateNormals();
+
+    // This is a quick way to create parameters for the voice. Trigger
+    // parameters are meant to be set only when the voice starts, i.e. they
+    // are expected to be constant within a voice instance. (You can actually
+    // change them while you are prototyping, but their changes will only be
+    // stored and aplied when a note is triggered.)
+
+    createInternalTriggerParameter("amplitude", 0.3, 0.0, 1.0);
+    createInternalTriggerParameter("frequency", 60, 20, 5000);
+    createInternalTriggerParameter("attackTime", 1.0, 0.01, 3.0);
+    createInternalTriggerParameter("releaseTime", 3.0, 0.1, 10.0);
+    createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
+
+    // Initalize MIDI device input
+  }
+
+  // The audio processing function
+  void onProcess(AudioIOData &io) override
+  {
+    // Get the values from the parameters and apply them to the corresponding
+    // unit generators. You could place these lines in the onTrigger() function,
+    // but placing them here allows for realtime prototyping on a running
+    // voice, rather than having to trigger a new voice to hear the changes.
+    // Parameters will update values once per audio callback because they
+    // are outside the sample processing loop.
+    mOsc.freq(getInternalParameterValue("frequency"));
+    mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
+    mAmpEnv.lengths()[2] = getInternalParameterValue("releaseTime");
+    mPan.pos(getInternalParameterValue("pan"));
+    while (io())
+    {
+      float s1 = mOsc() * mAmpEnv() * getInternalParameterValue("amplitude");
+      float s2;
+      mEnvFollow(s1);
+      mPan(s1, s1, s2);
+      io.out(0) += s1;
+      io.out(1) += s2;
+    }
+    // We need to let the synth know that this voice is done
+    // by calling the free(). This takes the voice out of the
+    // rendering chain
+    if (mAmpEnv.done() && (mEnvFollow.value() < 0.001f))
+      free();
+  }
+
+  // The graphics processing function
+  void onProcess(Graphics &g) override
+  {
+    a += 0.29;
+    b += 0.23;
+    timepose += 0.02;
+    // Get the paramter values on every video frame, to apply changes to the
+    // current instance
+    float frequency = getInternalParameterValue("frequency");
+    float amplitude = getInternalParameterValue("amplitude");
+    // Now draw
+    g.pushMatrix();
+    g.depthTesting(true);
+    g.lighting(true);
+    g.translate(note_position + note_direction * timepose);
+    g.rotate(a, Vec3f(0, 1, 0));
+    g.rotate(b, Vec3f(1));
+    g.scale( 0.3+mAmpEnv()*0.2, 0.3+mAmpEnv()*0.5, amplitude);
+    g.color(HSV(frequency / 1000, 0.5+mAmpEnv()*0.1, 0.3+0.5*mAmpEnv()));
+    g.draw(mMesh);
+    g.popMatrix();
+  }
+
+  // The triggering functions just need to tell the envelope to start or release
+  // The audio processing function checks when the envelope is done to remove
+  // the voice from the processing chain.
+  void onTriggerOn() override { 
+    float angle = getInternalParameterValue("frequency") / 200;
+    mAmpEnv.reset(); 
+    a = al::rnd::uniform();
+    b = al::rnd::uniform();
+    timepose = 0;
+    note_position = {0,0,0};
+    note_direction = {sin(angle), cos(angle) , 0};
+
+  }
+
+  void onTriggerOff() override { mAmpEnv.release(); }
+};
+
+// We make an app.
+class MyApp : public App, public MIDIMessageHandler
+{
+public:
+  // GUI manager for SineEnv voices
+  // The name provided determines the name of the directory
+  // where the presets and sequences are stored
+  SynthGUIManager<SineEnv> synthManager{"SineEnv"};
+  RtMidiIn midiIn; // MIDI input carrier
+
+  // This function is called right after the window is created
+  // It provides a grphics context to initialize ParameterGUI
+  // It's also a good place to put things that should
+  // happen once at startup.
+  void onCreate() override
+  {
+    navControl().active(false); // Disable navigation via keyboard, since we
+                                // will be using keyboard for note triggering
+    nav().pos(0, 0, 13);  
+
+    // Set sampling rate for Gamma objects from app's audio
+    gam::sampleRate(audioIO().framesPerSecond());
+
+    imguiInit();
+
+    // Play example sequence. Comment this line to start from scratch
+    synthManager.synthSequencer().playSequence("synth1.synthSequence");
+    synthManager.synthRecorder().verbose(true);
+  }
+
+  void onInit()
+  {
+    // Check for connected MIDI devices
+    if (midiIn.getPortCount() > 0)
+    {
+      // Bind ourself to the RtMidiIn object, to have the onMidiMessage()
+      // callback called whenever a MIDI message is received
+      MIDIMessageHandler::bindTo(midiIn);
+
+      // Open the last device found
+      unsigned int port = midiIn.getPortCount() - 1;
+      midiIn.openPort(port);
+      printf("Opened port to %s\n", midiIn.getPortName(port).c_str());
+    }
+    else
+    {
+      printf("Error: No MIDI devices found.\n");
+    }
+  }
+  // The audio callback function. Called when audio hardware requires data
+  void onSound(AudioIOData &io) override
+  {
+    synthManager.render(io); // Render audio
+  }
+
+  void onAnimate(double dt) override
+  {
+    // The GUI is prepared here
+    imguiBeginFrame();
+    // Draw a window that contains the synth control panel
+    synthManager.drawSynthControlPanel();
+    imguiEndFrame();
+  }
+
+  // The graphics callback function.
+  void onDraw(Graphics &g) override
+  {
+    g.clear();
+    // Render the synth's graphics
+    synthManager.render(g);
+
+    // GUI is drawn here
+    imguiDraw();
+  }
+
+  // This gets called whenever a MIDI message is received on the port
+  void onMIDIMessage(const MIDIMessage &m)
+  {
+    switch (m.type())
+    {
+    case MIDIByte::NOTE_ON:
+    {
+      int midiNote = m.noteNumber();
+      if (midiNote > 0 && m.velocity() > 0.001)
+      {
+        synthManager.voice()->setInternalParameterValue(
+            "frequency", ::pow(2.f, (midiNote - 69.f) / 12.f) * 432.f);
+        synthManager.voice()->setInternalParameterValue(
+            "attackTime", m.velocity());
+        synthManager.triggerOn(midiNote);
+        printf("On Note %u, Vel %f", m.noteNumber(), m.velocity());
+      }
+      else {
+        synthManager.triggerOff(midiNote);
+        printf("Off Note %u, Vel %f", m.noteNumber(), m.velocity());
+      }
+      break;
+    }
+    }
+  }
+  // Whenever a key is pressed, this function is called
+  bool onKeyDown(Keyboard const &k) override
+  {
+    if (ParameterGUI::usingKeyboard())
+    { // Ignore keys if GUI is using
+      // keyboard
+      return true;
+    }
+    if (k.shift())
+    {
+      // If shift pressed then keyboard sets preset
+      int presetNumber = asciiToIndex(k.key());
+      synthManager.recallPreset(presetNumber);
+    }
+    else
+    {
+      // Otherwise trigger note for polyphonic synth
+      int midiNote = asciiToMIDI(k.key());
+      if (midiNote > 0)
+      {
+        synthManager.voice()->setInternalParameterValue(
+            "frequency", ::pow(2.f, (midiNote - 69.f) / 12.f) * 432.f);
+        synthManager.triggerOn(midiNote);
+      }
+    }
+
+    return true;
+  }
+
+  // Whenever a key is released this function is called
+  bool onKeyUp(Keyboard const &k) override
+  {
+    int midiNote = asciiToMIDI(k.key());
+    if (midiNote > 0)
+    {
+      synthManager.triggerOff(midiNote);
+    }
+    return true;
+  }
+
+  void onExit() override { imguiShutdown(); }
+};
+
+int main()
+{
+  // Create app instance
+  MyApp app;
+
+  // Set up audio
+  app.configureAudio(48000., 512, 2, 0);
+  app.start();
+  return 0;
+}
---
+++ tutorials/audiovisual/04_FMVib_visual.cpp
@@ -0,0 +1,301 @@
+// MUS109IA & MAT276IA.
+// Spring 2022
+// Course Instrument 04. FM Vib-Visual-MIDI
+// Myungin Lee
+
+#include <cstdio>  // for printing to stdout
+
+#include "Gamma/Analysis.h"
+#include "Gamma/Effects.h"
+#include "Gamma/Envelope.h"
+#include "Gamma/Gamma.h"
+#include "Gamma/Oscillator.h"
+#include "Gamma/Types.h"
+#include "al/app/al_App.hpp"
+#include "al/graphics/al_Shapes.hpp"
+#include "al/scene/al_PolySynth.hpp"
+#include "al/scene/al_SynthSequencer.hpp"
+#include "al/ui/al_ControlGUI.hpp"
+#include "al/ui/al_Parameter.hpp"
+
+// using namespace gam;
+using namespace al;
+using namespace std;
+float tclock = 0;
+
+class FM : public SynthVoice {
+ public:
+  // Unit generators
+  gam::Pan<> mPan;
+  gam::ADSR<> mAmpEnv;
+  gam::ADSR<> mModEnv;
+  gam::EnvFollow<> mEnvFollow;
+  gam::ADSR<> mVibEnv;
+
+  gam::Sine<> car, mod, mVib;  // carrier, modulator sine oscillators
+  double a = 0;
+  double b = 0;
+  double timepose = 10;
+  Mesh ball;
+
+  // Additional members
+  float mDur;
+  float mModAmt = 50;
+  float mVibFrq;
+  float mVibDepth;
+  float mVibRise;
+
+  void init() override {
+    //      mAmpEnv.curve(0); // linear segments
+    mAmpEnv.levels(0, 1, 1, 0);
+    mModEnv.levels(0, 1, 1, 0);
+    mVibEnv.levels(0, 1, 1, 0);
+    //      mVibEnv.curve(0);
+    addSphere(ball, 1, 100, 100);
+    ball.decompress();
+    ball.generateNormals();
+
+    // We have the mesh be a sphere
+    createInternalTriggerParameter("freq", 440, 10, 4000.0);
+    createInternalTriggerParameter("amplitude", 0.5, 0.0, 1.0);
+    createInternalTriggerParameter("attackTime", 0.1, 0.01, 3.0);
+    createInternalTriggerParameter("releaseTime", 0.1, 0.1, 10.0);
+    createInternalTriggerParameter("sustain", 0.75, 0.1, 1.0);
+
+    // FM index
+    createInternalTriggerParameter("idx1", 0.01, 0.0, 10.0);
+    createInternalTriggerParameter("idx2", 7, 0.0, 10.0);
+    createInternalTriggerParameter("idx3", 5, 0.0, 10.0);
+
+    createInternalTriggerParameter("carMul", 1, 0.0, 20.0);
+    createInternalTriggerParameter("modMul", 1.0007, 0.0, 20.0);
+
+    createInternalTriggerParameter("vibRate1", 0.01, 0.0, 10.0);
+    createInternalTriggerParameter("vibRate2", 0.5, 0.0, 10.0);
+    createInternalTriggerParameter("vibRise", 0, 0.0, 10.0);
+    createInternalTriggerParameter("vibDepth", 0, 0.0, 10.0);
+
+    createInternalTriggerParameter("pan", 0.0, -1.0, 1.0);
+  }
+
+  //
+  void onProcess(AudioIOData& io) override {
+    updateFromParameters();
+    mVib.freq(mVibEnv());
+    float carBaseFreq =
+        getInternalParameterValue("freq") * getInternalParameterValue("carMul");
+    float modScale =
+        getInternalParameterValue("freq") * getInternalParameterValue("modMul");
+    float amp = getInternalParameterValue("amplitude");
+    while (io()) {
+      mVib.freq(mVibEnv());
+      car.freq((1 + mVib() * mVibDepth) * carBaseFreq +
+               mod() * mModEnv() * modScale);
+      float s1 = car() * mAmpEnv() * amp;
+      float s2;
+      mEnvFollow(s1);
+      mPan(s1, s1, s2);
+      io.out(0) += s1;
+      io.out(1) += s2;
+    }
+    if (mAmpEnv.done() && (mEnvFollow.value() < 0.001)) free();
+  }
+
+  void onProcess(Graphics& g) override {
+    a += 0.29;
+    b += 0.23;
+    timepose -= 0.06;
+    g.pushMatrix();
+    g.depthTesting(true);
+    g.lighting(true);
+    g.translate( timepose, getInternalParameterValue("freq") / 200 - 3 , -4);
+    g.rotate(mVib() + a, Vec3f(0, 1, 0));
+    g.rotate(mVibDepth + b, Vec3f(1));
+    float scaling = getInternalParameterValue("amplitude") / 10 ;
+    g.scale(scaling + getInternalParameterValue("modMul") / 10, scaling + getInternalParameterValue("carMul") / 30 , scaling + mEnvFollow.value() * 5);
+    g.color(HSV(getInternalParameterValue("modMul") / 20, getInternalParameterValue("carMul") / 20, 0.5 + getInternalParameterValue("attackTime")));
+    g.draw(ball);
+    g.popMatrix();
+
+  }
+
+  void onTriggerOn() override {
+    timepose = 10;
+    updateFromParameters();
+
+    float modFreq =
+        getInternalParameterValue("freq") * getInternalParameterValue("modMul");
+    mod.freq(modFreq);
+
+    mVibEnv.lengths()[0] = mDur * (1 - mVibRise);
+    mVibEnv.lengths()[1] = mDur * mVibRise;
+    mAmpEnv.reset();
+    mVibEnv.reset();
+    mModEnv.reset();
+  }
+  void onTriggerOff() override {
+    mAmpEnv.triggerRelease();
+    mModEnv.triggerRelease();
+    mVibEnv.triggerRelease();
+  }
+
+  void updateFromParameters() {
+    mModEnv.levels()[0] = getInternalParameterValue("idx1");
+    mModEnv.levels()[1] = getInternalParameterValue("idx2");
+    mModEnv.levels()[2] = getInternalParameterValue("idx2");
+    mModEnv.levels()[3] = getInternalParameterValue("idx3");
+
+    mAmpEnv.levels()[1] = 1.0;
+    // mAmpEnv.levels()[2] = 1.0;
+    mAmpEnv.levels()[2] = getInternalParameterValue("sustain");
+
+    mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
+    mModEnv.lengths()[0] = getInternalParameterValue("attackTime");
+
+    mAmpEnv.lengths()[3] = getInternalParameterValue("releaseTime");
+    mModEnv.lengths()[3] = getInternalParameterValue("releaseTime");
+
+    mAmpEnv.totalLength(getInternalParameterValue("dur"), 1);
+    mModEnv.lengths()[1] = mAmpEnv.lengths()[1];
+    mVibEnv.levels()[1] = getInternalParameterValue("vibRate1");
+    mVibEnv.levels()[2] = getInternalParameterValue("vibRate2");
+    mVibDepth = getInternalParameterValue("vibDepth");
+    mVibRise = getInternalParameterValue("vibRise");
+    mPan.pos(getInternalParameterValue("pan"));
+
+  }
+};
+
+class MyApp : public App, public MIDIMessageHandler {
+ public:
+  SynthGUIManager<FM> synthManager{"synth4Vib"};
+  RtMidiIn midiIn; // MIDI input carrier
+  bool showGUI = true;
+  //    ParameterMIDI parameterMIDI;
+  int midiNote;
+  float mVibFrq;
+  float mVibDepth;
+  float tscale = 1;
+
+  void onInit() override {
+    imguiInit();
+
+    navControl().active(false);  // Disable navigation via keyboard, since we
+                                 // will be using keyboard for note triggering
+    // Set sampling rate for Gamma objects from app's audio
+    gam::sampleRate(audioIO().framesPerSecond());
+    // Check for connected MIDI devices
+    if (midiIn.getPortCount() > 0) {
+      // Bind ourself to the RtMidiIn object, to have the onMidiMessage()
+      // callback called whenever a MIDI message is received
+      MIDIMessageHandler::bindTo(midiIn);
+
+      // Open the last device found
+      unsigned int port = midiIn.getPortCount() - 1;
+      midiIn.openPort(port);
+      printf("Opened port to %s\n", midiIn.getPortName(port).c_str());
+    } else {
+      printf("Error: No MIDI devices found.\n");
+    }
+  }
+
+  void onCreate() override {
+    // Play example sequence. Comment this line to start from scratch
+    //    synthManager.synthSequencer().playSequence("synth2.synthSequence");
+    synthManager.synthRecorder().verbose(true);
+    nav().pos(3, 0, 17);  
+  }
+
+  void onSound(AudioIOData& io) override {
+    synthManager.render(io);  // Render audio
+  }
+
+  void onAnimate(double dt) override {
+    tclock += dt * tscale;
+    imguiBeginFrame();
+    synthManager.drawSynthControlPanel();
+    imguiEndFrame();
+  }
+
+  void onDraw(Graphics& g) override {
+    g.clear();
+    synthManager.render(g);
+    // Draw GUI
+    if (showGUI){
+      imguiDraw();
+    }
+  }
+  void onMIDIMessage(const MIDIMessage &m)
+  {
+    switch (m.type())
+    {
+    case MIDIByte::NOTE_ON:
+    {
+      int midiNote = m.noteNumber();
+      if (midiNote > 0 && m.velocity() > 0.001)
+      {
+        synthManager.voice()->setInternalParameterValue(
+            "freq", ::pow(2.f, (midiNote - 69.f) / 12.f) * 432.f);
+        synthManager.voice()->setInternalParameterValue(
+            "attackTime", m.velocity());
+        synthManager.triggerOn(midiNote);
+      }
+      else
+      {
+        synthManager.triggerOff(midiNote);
+      }
+      break;
+    }
+    }
+  }
+  bool onKeyDown(Keyboard const& k) override {
+    if (ParameterGUI::usingKeyboard()) {  // Ignore keys if GUI is using them
+      return true;
+    }
+    if (k.shift()) {
+      // If shift pressed then keyboard sets preset
+      int presetNumber = asciiToIndex(k.key());
+      synthManager.recallPreset(presetNumber);
+    } else {
+      // Otherwise trigger note for polyphonic synth
+      int midiNote = asciiToMIDI(k.key());
+      if (midiNote > 0) {
+        synthManager.voice()->setInternalParameterValue(
+            "freq", ::pow(2.f, (midiNote - 69.f) / 12.f) * 432.f);
+        synthManager.triggerOn(midiNote);
+      }
+    }
+    switch (k.key())
+    {
+    case ']':
+      showGUI = !showGUI;
+      break;
+    case '-':
+      tscale -= 0.1;
+      break;
+    case '+':
+      tscale += 0.1;
+    break;
+    }
+    return true;
+  }
+
+  bool onKeyUp(Keyboard const& k) override {
+    int midiNote = asciiToMIDI(k.key());
+    if (midiNote > 0) {
+      synthManager.triggerOff(midiNote);
+    }
+    return true;
+  }
+
+  void onExit() override { imguiShutdown(); }
+};
+
+int main() {
+  MyApp app;
+
+  // Set up audio
+  app.configureAudio(48000., 512, 2, 0);
+
+  app.start();
+}
