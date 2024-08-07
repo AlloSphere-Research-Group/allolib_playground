b9a68b883e1a85e9436dd2a4c5ffcb18b301bf52
Joel Jaffe <joel@jaffesd.com>
2024-08-07 12:25:42 -0700

    Added `writeIndex` init to constructor for DelayLine
    Per Codacy warning.

--- tutorials/learn_digital_audio/06_Delay.cpp
+++ tutorials/learn_digital_audio/06_Delay.cpp
@@ -26,7 +26,7 @@ private:
   T damping = 0.f; // to emulate the infidelity of analog delays 
 
 public:
-  DelayLine(int sampleRate) : bufferSize(sampleRate * 2), sampleRate(sampleRate) {
+  DelayLine(int sampleRate) : bufferSize(sampleRate * 2), writeIndex(0), sampleRate(sampleRate) {
     for (int i = 0; i < bufferSize; i++) {buffer.push_back(0);}
     loPass.setAlpha(damping);
   }
